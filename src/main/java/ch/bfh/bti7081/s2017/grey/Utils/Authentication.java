fce9b50060e0329b39beb626f8d9a1fdacd3b0e4
Joel von Allmen <joelrandy.vonallmen@students.bfh.ch>
2017-05-05 12:01:44 +0200

    implement interfaces for Authentication

---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/Utils/Authentication.java
@@ -0,0 +1,20 @@
+package ch.bfh.bti7081.s2017.grey.Utils;
+
+/**
+ * Created by jo-ra on 05.05.2017.
+ */
+public class Authentication {
+
+    /**
+     * Authenticate user by username and password
+     * compare with database
+     *
+     * @param username username from user
+     * @param password password used by user
+     * @return {Boolean}
+     */
+    public static Boolean authenticate(String username, String password) {
+        // TODO check with database
+        return false;
+    }
+}
