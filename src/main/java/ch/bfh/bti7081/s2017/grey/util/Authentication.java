823e55f4b43e8f6fceeaada9883b20f110c33fcd
Joel von Allmen <joelrandy.vonallmen@students.bfh.ch>
2017-05-08 18:04:37 +0200

    using dao for staff

--- src/main/java/ch/bfh/bti7081/s2017/grey/util/Authentication.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/util/Authentication.java
@@ -1,20 +1,27 @@
 package ch.bfh.bti7081.s2017.grey.util;
 
+import ch.bfh.bti7081.s2017.grey.database.entity.Staff;
+
 /**
  * Created by jo-ra on 05.05.2017.
  */
 public class Authentication {
 
     /**
-     * Authenticate user by username and password
+     * Authenticate user by login and password
      * compare with database
      *
-     * @param username username from user
+     * @param login login from user
      * @param password password used by user
      * @return {Boolean}
      */
-    public static Boolean authenticate(String username, String password) {
-        // TODO check with database
-        return true;
+    public static Boolean authenticate(String login, String password) {
+        Staff staff = StaffDao.getStaffByUsername(login);
+        return  (staff.getPwhash().equals(Authentication.createHash(password)));
+    }
+
+
+    public static String createHash(String plain)  {
+        return String.valueOf(plain.hashCode());
     }
 }
