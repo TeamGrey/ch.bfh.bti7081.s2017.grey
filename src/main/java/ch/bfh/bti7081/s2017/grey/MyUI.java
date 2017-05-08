8be504dac703a3a5b57b59e14dbb13e2ede19c6f
hdrodel <=hdrodel@gmail.com>
2017-05-08 16:31:48 +0200

    View with datefields, simple appointment for testing

--- src/main/java/ch/bfh/bti7081/s2017/grey/MyUI.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/MyUI.java
@@ -1,47 +1,54 @@
-package ch.bfh.bti7081.s2017.grey;
+        package ch.bfh.bti7081.s2017.grey;
 
-import javax.servlet.annotation.WebServlet;
+        import javax.servlet.annotation.WebServlet;
 
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+        import com.vaadin.annotations.Theme;
+        import com.vaadin.navigator.Navigator;
+        import com.vaadin.server.Page;
+        import com.vaadin.server.VaadinRequest;
+        import com.vaadin.server.VaadinServlet;
+        import com.vaadin.ui.Button;
+        import com.vaadin.ui.Label;
+        import com.vaadin.ui.Notification;
+        import com.vaadin.ui.TextField;
+        import com.vaadin.ui.UI;
+        import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is the application entry point. A UI may either represent a browser window
  * (or tab) or some part of a html page where a Vaadin application is embedded.
  * <p>
- * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be
  * overridden to add component to the user interface and initialize non-component functionality.
  */
+@SuppressWarnings("serial")
 @Theme("mytheme")
 public class MyUI extends UI {
 
+    @WebServlet(value = "/*", asyncSupported = true)
+    public static class Servlet extends VaadinServlet {
+    }
+
     @Override
-    protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
-        final TextField name = new TextField();
-        name.setCaption("Type your name here:");
+    protected void init(VaadinRequest request) {
+        new Navigator(this, this);
 
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue() 
-                    + ", it works!"));
-        });
-        
-        layout.addComponents(name, button);
-        
-        setContent(layout);
+        getNavigator().addView(AppointmentView.NAME, AppointmentView.class);
+        getNavigator().setErrorView(Appointmentview.class);
+
+        router("");
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
+    private void router(String route){
+        Notification.show(route);
+        if(getSession().getAttribute("user") != null){
+
+        }else{
+            getNavigator().navigateTo(AppointmentView.NAME);
+        }
     }
+
 }
+
+
+
