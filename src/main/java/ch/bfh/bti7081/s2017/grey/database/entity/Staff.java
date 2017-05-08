926431ba7657d7324117909724672deb00a1198d
Quentin Chiquet <quentin_ch@hotmail.com>
2017-05-08 17:38:56 +0200

    JPA config

---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/entity/Staff.java
@@ -0,0 +1,93 @@
+package ch.bfh.bti7081.s2017.grey.database.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.sql.Timestamp;
+
+/**
+ * @Author Quentin
+ */
+@Entity
+@Table
+public class Staff {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String pwhash;
+    private String login;
+    private int roleId;
+    private Timestamp created;
+    private Timestamp changed;
+
+    public Staff(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPwhash() {
+        return pwhash;
+    }
+
+    public void setPwhash(String pwhash) {
+        this.pwhash = pwhash;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getChanged() {
+        return changed;
+    }
+
+    public void setChanged(Timestamp changed) {
+        this.changed = changed;
+    }
+}
