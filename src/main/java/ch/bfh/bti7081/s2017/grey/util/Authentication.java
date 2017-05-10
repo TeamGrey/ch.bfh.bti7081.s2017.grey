7e0347e810cdd4ed6dbe8766b82995228c18319c
Joel von Allmen <joelrandy.vonallmen@students.bfh.ch>
2017-05-10 19:32:10 +0200

    serveral changes

--- src/main/java/ch/bfh/bti7081/s2017/grey/util/Authentication.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/util/Authentication.java
@@ -3,6 +3,7 @@ package ch.bfh.bti7081.s2017.grey.util;
 import ch.bfh.bti7081.s2017.grey.database.dao.StaffDao;
 import ch.bfh.bti7081.s2017.grey.database.entity.Staff;
 
+
 /**
  * Service for User authentication
  *
@@ -14,13 +15,24 @@ public class Authentication {
      * Authenticate user by login and password
      * compare with database
      *
-     * @param login login from user
+     * @param login user reference
      * @param password password used by user
      * @return {Boolean}
      */
     public static Boolean authenticate(String login, String password) {
         Staff staff = StaffDao.getStaffByLogin(login);
-        return  (staff.getPwhash().equals(Authentication.createHash(password)));
+        return authenticate(staff, password);
+    }
+
+    /**
+     * Authenticate user by entity and password
+     *
+     * @param user user entity
+     * @param password password used by user
+     * @return {Boolean}
+     */
+    public static Boolean authenticate(Staff user, String password){
+        return (user.getPwhash().equals(Authentication.createHash(password)));
     }
 
 
