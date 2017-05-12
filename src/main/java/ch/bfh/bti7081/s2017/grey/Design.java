d1b64c62fa0c91cd746f792122556f53c9755561
kenboy192 <kenlestander@hotmail.com>
2017-05-12 11:10:15 +0200

    Loout-Button

--- src/main/java/ch/bfh/bti7081/s2017/grey/Design.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/Design.java
@@ -2,11 +2,16 @@ package ch.bfh.bti7081.s2017.grey;
 
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
+import com.vaadin.server.Page;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickListener;
 
 public class Design extends VerticalLayout implements View {
 	private static final long serialVersionUID = 1L;
@@ -15,18 +20,36 @@ public class Design extends VerticalLayout implements View {
 	private Component lHeader = new Label("Header");
 	private Component l2Header = new Label("Header2");
 	private Component lFooter = new Label("Footer");
+	private Button logout = new Button("Logout");
 
 	public Design(){
 		mainlayout.setSizeFull();
 		lHeader.setSizeFull();
 		lFooter.setSizeFull();
+		
+		logout.addClickListener(new ClickListener() {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+				VaadinSession.getCurrent().close();	
+				Notification.show("Logging out...", Notification.Type.WARNING_MESSAGE);
+				Page.getCurrent().setUriFragment("");
+			}
+		});
 	}
 	
 	public Design insertContent(Component content){
-		HorizontalLayout test = new HorizontalLayout();
-		test.addComponents(lHeader, l2Header);
+		HorizontalLayout header = new HorizontalLayout();
+		if(VaadinSession.getCurrent().getAttribute("user") != null){
+			header.addComponents(lHeader, l2Header, logout);
+		}
+		else{
+			header.addComponents(lHeader, l2Header);
+		}
 		
-		mainlayout.addComponent(test, "top");
+		
+		mainlayout.addComponent(header, "top");
 		mainlayout.addComponent(content, "center");
 		mainlayout.addComponent(lFooter, "bottom");
 		
