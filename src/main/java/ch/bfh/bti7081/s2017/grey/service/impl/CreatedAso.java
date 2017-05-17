7177c3c632db93f33fda7013e0c2854da3d5099f
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-17 18:06:03 +0200

    add implementations for state operations (some are still dummies

---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/service/impl/CreatedAso.java
@@ -0,0 +1,34 @@
+package ch.bfh.bti7081.s2017.grey.service.impl;
+
+import ch.bfh.bti7081.s2017.grey.database.entity.Appointment;
+import ch.bfh.bti7081.s2017.grey.database.entity.AppointmentStatus;
+import ch.bfh.bti7081.s2017.grey.service.AppointmentStatusOperations;
+
+/**
+ * Created by gabor on 17/05/17.
+ */
+public class CreatedAso implements AppointmentStatusOperations{
+    @Override
+    public AppointmentStatus create(Appointment appointment) {
+        // no operation needed here
+        return appointment.getStatus();
+    }
+
+    @Override
+    public AppointmentStatus delay(Appointment appointment) {
+        // TODO delay operations
+        return AppointmentStatus.DELAYED;
+    }
+
+    @Override
+    public AppointmentStatus cancel(Appointment appointment) {
+        // TODO cancel operations
+        return AppointmentStatus.CANCELED;
+    }
+
+    @Override
+    public AppointmentStatus finish(Appointment appointment) {
+        // TODO finish operations
+        return AppointmentStatus.FINISHED;
+    }
+}
---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/service/impl/DelayedAso.java
@@ -0,0 +1,30 @@
+package ch.bfh.bti7081.s2017.grey.service.impl;
+
+import ch.bfh.bti7081.s2017.grey.database.entity.Appointment;
+import ch.bfh.bti7081.s2017.grey.database.entity.AppointmentStatus;
+import ch.bfh.bti7081.s2017.grey.service.AppointmentStatusOperations;
+
+/**
+ * Created by gabor on 17/05/17.
+ */
+public class DelayedAso implements AppointmentStatusOperations {
+    @Override
+    public AppointmentStatus create(Appointment appointment) {
+        return null;
+    }
+
+    @Override
+    public AppointmentStatus delay(Appointment appointment) {
+        return null;
+    }
+
+    @Override
+    public AppointmentStatus cancel(Appointment appointment) {
+        return null;
+    }
+
+    @Override
+    public AppointmentStatus finish(Appointment appointment) {
+        return null;
+    }
+}
