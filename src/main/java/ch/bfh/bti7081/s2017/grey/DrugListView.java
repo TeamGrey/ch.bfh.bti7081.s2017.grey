9e5d6f6e1bcb15c68e7fe4d5ec43bf3a31ea74f0
sheggi <thesheggi@gmail.com>
2017-05-18 14:44:10 +0200

    add views for medication

---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/DrugListView.java
@@ -0,0 +1,23 @@
+package ch.bfh.bti7081.s2017.grey;
+
+import com.vaadin.ui.VerticalLayout;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DrugListView extends VerticalLayout {
+    private List<DrugView> drugs;
+
+    public DrugListView() {
+        super();
+        drugs = new LinkedList<DrugView>();
+    }
+
+    public DrugView addDrug(String name) {
+        DrugView drug = new DrugView();
+        drug.setName(name);
+        drugs.add(drug);
+        addComponent(drug);
+        return drug;
+    }
+}
---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/DrugScreenPresenter.java
@@ -0,0 +1,33 @@
+package ch.bfh.bti7081.s2017.grey;
+
+import com.vaadin.navigator.View;
+import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.VerticalLayout;
+
+public class DrugScreenPresenter extends VerticalLayout implements View {
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "DrugScreen";
+
+    private DrugListView drugs = null;
+    private Design design = null;
+
+    public DrugScreenPresenter() {
+        drugs = new DrugListView();
+
+        design = new Design();
+        design.insertContent(drugs);
+
+        // TODO get drug list from DB
+        for (int i = 0; i < 4; i++) {
+            drugs.addDrug("Drug Nr:" + i);
+        }
+
+        addComponent(design);
+    }
+
+
+    @Override
+    public void enter(ViewChangeListener.ViewChangeEvent event) {
+        MyUI.loggedIn();
+    }
+}
