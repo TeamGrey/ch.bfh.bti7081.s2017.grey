e645c7255120b05ff3fc1499d910d7c2e3ca0e42
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-29 21:31:25 +0200

    replace daos with generic interfaces

--- src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/DrugDao.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/DrugDao.java
@@ -1,61 +1,9 @@
 package ch.bfh.bti7081.s2017.grey.database.dao;
 
 import ch.bfh.bti7081.s2017.grey.database.entity.Drug;
-import ch.bfh.bti7081.s2017.grey.database.util.EntityManagerSingleton;
-
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.sql.Timestamp;
-import java.time.Instant;
-import java.util.List;
 
 /**
- * @Author Quentin
+ * Created by gabor on 29/05/17.
  */
-public class DrugDao {
-
-    private EntityManager entityManager;
-
-    public DrugDao() {
-        this.entityManager = EntityManagerSingleton.getInstance();
-    }
-
-    public Drug getDrugById(long id) {
-        Drug drug =entityManager.find(Drug.class, id);
-        return drug;
-    }
-
-    public List<Drug> getAllDrugs() {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Drug> c = criteriaBuilder.createQuery(Drug.class);
-        Root<Drug> drug = c.from(Drug.class);
-        c.select(drug);
-
-        TypedQuery<Drug> query = entityManager.createQuery(c);
-        return query.getResultList();
-    }
-
-    public void createDrug(String name) {
-        entityManager.getTransaction().begin();
-
-        Instant instant = Instant.now();
-        Drug drug = new Drug();
-        drug.setName(name);
-        drug.setCreated(new Timestamp(instant.toEpochMilli()));
-        drug.setChanged(new Timestamp(instant.toEpochMilli()));
-        entityManager.persist(drug);
-        entityManager.getTransaction().commit();
-    }
-
-    public void updateDrugName(long id, String newName) {
-        Drug drug = entityManager.find(Drug.class, id);
-        drug.setName(newName);
-        Instant instant = Instant.now();
-        drug.setChanged(new Timestamp(instant.toEpochMilli()));
-        entityManager.getTransaction().commit();
-    }
-
+public interface DrugDao extends GenericDao<Drug>{
 }
--- src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/HabitDao.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/HabitDao.java
@@ -1,38 +1,9 @@
 package ch.bfh.bti7081.s2017.grey.database.dao;
 
 import ch.bfh.bti7081.s2017.grey.database.entity.Habit;
-import ch.bfh.bti7081.s2017.grey.database.util.EntityManagerSingleton;
-
-import javax.persistence.EntityManager;
-import java.sql.Timestamp;
-import java.time.Instant;
 
 /**
- * @Author Quentin
+ * Created by gabor on 29/05/17.
  */
-public class HabitDao {
-
-    private EntityManager entityManager;
-
-    public HabitDao() {
-        entityManager = EntityManagerSingleton.getInstance();
-    }
-
-    public Habit findHabitById(long id) {
-        return entityManager.find(Habit.class, id);
-    }
-
-    public void createHabit(String name) {
-        entityManager.getTransaction().begin();
-        Instant instant = Instant.now();
-        Timestamp timestamp = new Timestamp(instant.toEpochMilli());
-
-        Habit habit = new Habit();
-        habit.setName(name);
-        habit.setCreated(timestamp);
-        habit.setChanged(timestamp);
-
-        entityManager.persist(habit);
-        entityManager.getTransaction().commit();
-    }
+public interface HabitDao extends GenericDao<Habit> {
 }
