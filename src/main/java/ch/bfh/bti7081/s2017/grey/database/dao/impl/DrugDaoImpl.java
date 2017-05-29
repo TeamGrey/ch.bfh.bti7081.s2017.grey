eaf80b2636f29d3f984df3f9249661e67e0b12d7
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-29 21:31:47 +0200

    implement the daos

---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/impl/DrugDaoImpl.java
@@ -0,0 +1,10 @@
+package ch.bfh.bti7081.s2017.grey.database.dao.impl;
+
+import ch.bfh.bti7081.s2017.grey.database.dao.DrugDao;
+import ch.bfh.bti7081.s2017.grey.database.entity.Drug;
+
+/**
+ * Created by gabor on 29/05/17.
+ */
+public class DrugDaoImpl extends GenericDaoImpl<Drug> implements DrugDao {
+}
---
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/impl/RoleDaoImpl.java
@@ -0,0 +1,10 @@
+package ch.bfh.bti7081.s2017.grey.database.dao.impl;
+
+import ch.bfh.bti7081.s2017.grey.database.dao.RoleDao;
+import ch.bfh.bti7081.s2017.grey.database.entity.Role;
+
+/**
+ * Created by gabor on 29/05/17.
+ */
+public class RoleDaoImpl extends GenericDaoImpl<Role> implements RoleDao {
+}
