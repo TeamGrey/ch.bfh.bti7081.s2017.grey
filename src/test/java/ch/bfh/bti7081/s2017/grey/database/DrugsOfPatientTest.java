ed606174ec4c0067eed6bda030b3dc11496000f8
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-29 22:52:21 +0200

    adjust tests to use new dao and test services instead of dao

--- src/test/java/ch/bfh/bti7081/s2017/grey/database/DrugsOfPatientTest.java
+++ src/test/java/ch/bfh/bti7081/s2017/grey/database/DrugsOfPatientTest.java
@@ -1,7 +1,8 @@
 package ch.bfh.bti7081.s2017.grey.database;
 
-import ch.bfh.bti7081.s2017.grey.database.dao.PatientDao;
 import ch.bfh.bti7081.s2017.grey.database.entity.*;
+import ch.bfh.bti7081.s2017.grey.service.PatientService;
+import ch.bfh.bti7081.s2017.grey.service.impl.PatientServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,12 +15,12 @@ import static org.junit.Assert.assertEquals;
  */
 public class DrugsOfPatientTest {
     private Patient patient;
-    private PatientDao patDao;
+    private PatientService patientService;
 
     @Before
     public void setup() {
-        patDao = new PatientDao();
-        patient = patDao.getPatientById(4);
+        patientService = new PatientServiceImpl();
+        patient = patientService.getPatientById(4);
     }
 
     @Test
