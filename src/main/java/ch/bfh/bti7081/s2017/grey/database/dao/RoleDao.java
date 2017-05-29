77d5dbaf28bd55de06920660295188d1fd38a5d3
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-29 21:31:25 +0200

    replace daos with generic interfaces

--- src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/RoleDao.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/database/dao/RoleDao.java
@@ -1,41 +1,9 @@
 package ch.bfh.bti7081.s2017.grey.database.dao;
 
-import ch.bfh.bti7081.s2017.grey.database.util.EntityManagerSingleton;
 import ch.bfh.bti7081.s2017.grey.database.entity.Role;
 
-import javax.persistence.EntityManager;
-import java.sql.Timestamp;
-import java.time.Instant;
-
 /**
- * Data access object for {@link Role} Entity
- * @Author Quentin
+ * Created by gabor on 29/05/17.
  */
-public class RoleDao {
-
-    private EntityManager entityManager;
-
-    public RoleDao () {
-        entityManager = EntityManagerSingleton.getInstance();
-    }
-
-    public  Role getRoleById(long id) {
-        Role role = entityManager.find( Role.class, id );
-        return role;
-    }
-
-
-    public void createRole(String name) {
-        entityManager.getTransaction().begin();
-
-        Instant instant = Instant.now();
-
-        Role role = new Role();
-        role.setName(name);
-        role.setCreated(new Timestamp(instant.toEpochMilli()));
-        role.setChanged(new Timestamp(instant.toEpochMilli()));
-
-        entityManager.persist(role);
-        entityManager.getTransaction().commit();
-    }
+public interface RoleDao extends GenericDao<Role> {
 }
