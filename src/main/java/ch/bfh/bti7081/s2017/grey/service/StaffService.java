17c66bceb49688d3ed4e0a0c99468313362d3952
Gabor Tanz <gabor.tanz@gmail.com>
2017-05-31 20:05:02 +0200

    remove useless public on interfaces

--- src/main/java/ch/bfh/bti7081/s2017/grey/service/StaffService.java
+++ src/main/java/ch/bfh/bti7081/s2017/grey/service/StaffService.java
@@ -7,8 +7,7 @@ import ch.bfh.bti7081.s2017.grey.database.entity.Staff;
  * @Author Quentin
  */
 public interface StaffService {
-
-    public Staff findStaffByLogin(String login);
-    public Staff createStaff(String firstname, String lastname, String login, String pwhash, Role role);
-    public void deleteStaff(Staff staff);
+    Staff findStaffByLogin(String login);
+    Staff createStaff(String firstname, String lastname, String login, String pwhash, Role role);
+    void deleteStaff(Staff staff);
 }
